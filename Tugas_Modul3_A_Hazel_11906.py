e6663949c5e9472cb145a4ef8fc3cba40b042b27
GabrielHazel <220711906@students.uajy.ac.id>
2024-10-05 21:33:58 +0700

    Update Tugas_Modul3_A_Hazel_11906.py

--- Tugas_Modul3_A_Hazel_11906.py
+++ Tugas_Modul3_A_Hazel_11906.py
@@ -73,8 +73,8 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload:</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'D:\perkuliahan\semester 5\PMDPM\Pertemuan 6\Model Suprevised Learning (Praktek)\Tugas3_A_11906'
-    model_path = os.path.join(model_directory, r'Ridge_IPK_model.pkl')
+    
+    model_path = r'Ridge_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
